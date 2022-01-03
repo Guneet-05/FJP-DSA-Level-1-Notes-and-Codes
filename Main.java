35415e677e3fd3e893b7b3a8109ab5409a01c1a6
Guneet Malhotra <guneetmalhotra01@gmail.com>
2022-01-03 23:48:30 +0530

    tnc first class notes added

--- Main.java
+++ Main.java
@@ -6,42 +6,64 @@ public static void main(String[] args) throws Exception {
     // write your code here
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-    
+
     int[] arr = new int[n];
-    
-    for(int i=0;i<n;i++) {
+
+    for(int i=0;i<arr.length;i++) {
         arr[i] = scn.nextInt();
     }
+
     scn.close();
-    
-    barChart(arr);
+    // display(arr, 0);
+    // System.out.println("\n");
+    // displayReverse(arr, 0);
+    // System.out.println(max(arr, 0));
+    System.out.println(firstIndex(arr, 1, 0));
+    System.out.println(lastIndex(arr, 1, arr.length - 1));
  }
- 
- public static void barChart(int[] arr) {
-    int Max = max(arr);
-    int n = arr.length;
-    
-    for(int i=Max; i>=1 ;i--) {
-        for(int j=0;j<n;j++) {
-            if(arr[j] >= i) {
-                System.out.print("*\t");
-            } else {
-                System.out.print("\t");
-            }
-        }
-        System.out.println();
-    }
+
+ public static void display(int[] arr, int idx) {
+
+    if(idx == arr.length) return;
+
+    System.out.println(arr[idx]);
+    display(arr,idx+1);
  }
- 
- public static int max(int[] arr) {
+
+ public static void displayReverse(int[] arr, int idx) {
+    if(idx == arr.length) return;
     
-    int max = Integer.MIN_VALUE;
-    for(int i=0;i<arr.length;i++) {
-        if(arr[i] > max) {
-            max = arr[i];
-        }
+    displayReverse(arr,idx+1);
+    System.out.println(arr[idx]);
+ }
+
+ public static int max(int[] arr, int idx) {
+
+    if(idx == arr.length) return Integer.MIN_VALUE;
+
+    int maxOfROE = max(arr,idx + 1);
+    if(arr[idx] > maxOfROE) return arr[idx];
+
+    return maxOfROE;
+ }
+
+ public static int firstIndex(int[] arr,int target, int idx) {
+    
+    if(idx == arr.length) {
+        return -1;
     }
-    return max;
+
+    if(arr[idx] == target) return idx;
+
+    return firstIndex(arr, target, idx + 1);
+ }
+
+ public static int lastIndex(int[] arr, int target, int idx) {
+
+    if(idx == -1) return idx;
+
+    if(arr[idx] == target) return idx;
+    return lastIndex(arr,target,idx-1);
  }
 
 }
